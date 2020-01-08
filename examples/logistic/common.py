744fe406dfd1c80c08d86e0d83e8e7258d9b9459
Cheng (Vince) Hong <35251608+vincehong@users.noreply.github.com>
2020-01-08 12:00:21 +0800

    enable providing data from local file

--- examples/logistic/common.py
+++ examples/logistic/common.py
@@ -73,86 +73,52 @@ class LogisticRegression:
 
 class DataOwner:
   """Contains code meant to be executed by a data owner Player."""
-  def __init__(
-      self,
-      player_name,
-      num_features,
-      training_set_size,
-      test_set_size,
-      batch_size
-  ):
+  def __init__(self, player_name, local_data_file, data_schema,
+           header=False, index=False, field_delim=',', na_values=['nan'], batch_size=128):
     self.player_name = player_name
-    self.num_features = num_features
-    self.training_set_size = training_set_size
-    self.test_set_size = test_set_size
+    self.local_data_file = local_data_file
+    self.data_schema = data_schema
     self.batch_size = batch_size
-    self.train_initializer = None
-    self.test_initializer = None
+    self.header = header
+    self.index = index
+    self.na_values = na_values
+    self.field_delim = field_delim
+
+  def provide_data(self):
+
+    def decode(line):
+      fields = tf.string_split([line], self.field_delim).values
+      if self.index: # Skip index
+        fields = fields[1:]
+      fields = tf.regex_replace(fields, '|'.join(self.na_values), 'nan')
+      fields = tf.string_to_number(fields, tf.float32)
+      return fields
+
+    def fill_na(fields, fill_values):
+      fields = tf.where(tf.is_nan(fields), fill_values, fields)
+      return fields
+
+    dataset = tf.data.TextLineDataset(self.local_data_file)
+    if self.header: # Skip header
+      dataset = dataset.skip(1)
+    dataset = dataset\
+        .map(decode)\
+        .map(lambda x: fill_na(x, self.data_schema.field_defaults))\
+        .repeat()\
+        .batch(self.batch_size)
+    iterator = dataset.make_one_shot_iterator()
+    batch = iterator.get_next()
+    batch = tf.reshape(batch, [self.batch_size, self.data_schema.field_num])
+    return batch
+
+class DataSchema:
+  def __init__(self, field_types, field_defaults):
+    self.field_types = field_types
+    self.field_defaults = field_defaults
 
   @property
-  def initializer(self):
-    return tf.group(self.train_initializer, self.test_initializer)
-
-  @tfe.local_computation
-  def provide_training_data(self):
-    """Preprocess training dataset
-
-    Return single batch of training dataset
-    """
-    def norm(x, y):
-      return tf.cast(x, tf.float32), tf.expand_dims(y, 0)
-
-    x_raw = tf.random.uniform(
-        minval=-.5,
-        maxval=.5,
-        shape=[self.training_set_size, self.num_features])
-
-    y_raw = tf.cast(tf.reduce_mean(x_raw, axis=1) > 0, dtype=tf.float32)
-
-    train_set = tf.data.Dataset.from_tensor_slices((x_raw, y_raw)) \
-        .map(norm) \
-        .repeat() \
-        .shuffle(buffer_size=self.batch_size) \
-        .batch(self.batch_size)
-
-    train_set_iterator = train_set.make_initializable_iterator()
-    self.train_initializer = train_set_iterator.initializer
-
-    x, y = train_set_iterator.get_next()
-    x = tf.reshape(x, [self.batch_size, self.num_features])
-    y = tf.reshape(y, [self.batch_size, 1])
-
-    return x, y
-
-  @tfe.local_computation
-  def provide_testing_data(self):
-    """Preprocess testing dataset
-
-    Return single batch of testing dataset
-    """
-    def norm(x, y):
-      return tf.cast(x, tf.float32), tf.expand_dims(y, 0)
-
-    x_raw = tf.random.uniform(
-        minval=-.5,
-        maxval=.5,
-        shape=[self.test_set_size, self.num_features])
-
-    y_raw = tf.cast(tf.reduce_mean(x_raw, axis=1) > 0, dtype=tf.float32)
-
-    test_set = tf.data.Dataset.from_tensor_slices((x_raw, y_raw)) \
-        .map(norm) \
-        .batch(self.test_set_size)
-
-    test_set_iterator = test_set.make_initializable_iterator()
-    self.test_initializer = test_set_iterator.initializer
-
-    x, y = test_set_iterator.get_next()
-    x = tf.reshape(x, [self.test_set_size, self.num_features])
-    y = tf.reshape(y, [self.test_set_size, 1])
-
-    return x, y
-
+  def field_num(self):
+    return len(self.field_types)
 
 class ModelOwner:
   """Contains code meant to be executed by a model owner Player."""
@@ -181,3 +147,4 @@ class PredictionClient:
   @tfe.local_computation
   def receive_output(self, result):
     return tf.print("Result on {}:".format(self.player_name), result)
+
